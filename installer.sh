3e3a636243d90bb29a276735fa169521906e94a4
Peter Mosmans <support@go-forward.net>
2023-05-31 12:05:19 +0200

    feat: add installer script to start up demo lab

---
+++ installer.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Start up demo lab
+
+# Copyright (C) 2023 Peter Mosmans [Go Forward]
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e
+
+COMPOSE=$(which docker-compose)
+compose_file="docker-compose.yml"
+
+# shellcheck disable=SC1091
+source .ENV
+
+echo "Starting the services"
+sudo "$COMPOSE" -f "$compose_file" up --detach
+
+echo "Waiting on SonarQube to come up online"
+while [[ $(sudo "$COMPOSE" -f "$compose_file" logs sonarqube 2>/dev/null | grep -q "SonarQube is operational"; echo $?) -ne 0 ]]; do
+    sleep 1
+done
+echo "Changing default admin password to ${SONARQUBE_PASSWORD}"
+curl -u admin:admin -X POST "http://localhost:9000/api/users/change_password?login=admin&previousPassword=admin&password=${SONARQUBE_PASSWORD}"
+echo "You now can log in to SonarQube at http://localhost:9000"
+echo "Waiting for GitLab to start up..."
+while [[ $(sudo "$COMPOSE" -f "$compose_file" logs gitlab 2>/dev/null | grep -q "Server initialized"; echo $?) -ne 0 ]]; do
+    sleep 1
+done
+echo "You can now log in to GitLab at http://localhost:8080"
+
