ecedc2ed893d678962542ce5971275b8bb3edb7f
Peter Mosmans <support@go-forward.net>
2024-01-17 11:40:19 +0100

    fix: ensure that gitlab service name is being used for registration

--- register-runners.sh
+++ register-runners.sh
@@ -13,11 +13,13 @@ source .env
 # Ensure that all variables are defined
 set -u
 token=$1
+
+# Note that the hard-coded service name gitlab is being used
 for runner in $(docker-compose ps | awk '/-runner/{print $1}'); do
   echo "Trying to register runner on $runner"
   docker exec -it "$runner" /bin/bash -c "/usr/bin/gitlab-runner register \
 --non-interactive \
---url http://${GITLAB_HOSTNAME}:${GITLAB_PORT} \
+--url http://gitlab:${GITLAB_PORT} \
 --description $runner \
 --docker-image docker:${DIND_VERSION} \
 --docker-network-mode ${DEMO_NAME}-network \
